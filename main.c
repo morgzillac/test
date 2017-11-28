0792ab419edad95bf8e1029b71e5b36d004ecb9a
Morgan Cheng <mchengcat@hotmail.com>
2017-11-28 15:21:42 +0800

    first submission

---
+++ main.c
@@ -0,0 +1,8 @@
+//
+//  main.c
+//  test
+//
+//  Created by Morgan Cheng on 11/28/17.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,13 @@
+//
+//  main.h
+//  test
+//
+//  Created by Morgan Cheng on 11/28/17.
+//
+
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+#endif /* main_h */
